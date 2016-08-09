389f8d9dca63f3408e9b7d45f2a26ec461eda622
堇花 <wenhua.ywh@alibaba-inc.com>
2016-08-09 17:44:53 +0800

    VelocityEngine used to generate code.

--- compile-time-processors/src/main/java/cn/mutils/compiletime/PageActionProcessor.java
+++ compile-time-processors/src/main/java/cn/mutils/compiletime/PageActionProcessor.java
@@ -1,10 +1,13 @@
 package cn.mutils.compiletime;
 
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+
+import java.io.StringWriter;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -17,7 +20,6 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
-import javax.tools.JavaFileObject;
 
 /**
  * Created by wenhua.ywh on 2016/8/5.
@@ -25,6 +27,34 @@ import javax.tools.JavaFileObject;
 @SupportedAnnotationTypes("cn.mutils.compiletime.PageAction")
 @SupportedSourceVersion(SourceVersion.RELEASE_6)
 public class PageActionProcessor extends AbstractProcessor {
+
+    private static final String TAG = PageActionProcessor.class.getSimpleName();
+
+    private static final String TEMPLATE_MAP_LIBRARY = "// Generate code from " + TAG + ". Do not modify!\n" +
+            "// Email: wavinsun@qq.com\n" +
+            "package $PACKAGE_NAME;\n\n" +
+            "import java.util.HashMap;\n\n" +
+            "public final class $CLASS_NAME extends HashMap<String, Class<?>> {\n\n" +
+            "  public $CLASS_NAME() {\n" +
+            "#foreach($action in ${ACTION_MAP.keySet()})" +
+            "    put(\"${action}\", ${ACTION_MAP.get(${action})}.class);\n" +
+            "#end\n" +
+            "  }\n\n}\n";
+    private static final String TEMPLATE_MAP_APPLICATION = "// Generated code from " + TAG + ". Do not modify!\n" +
+            "// Email: wavinsun@qq.com\n" +
+            "package $PACKAGE_NAME;\n\n" +
+            "import java.util.HashMap;\n\n" +
+            "public final class $CLASS_NAME implements $INTERFACE_NAME {\n\n" +
+            "  private static final HashMap<String, Class<?>> sActionMap = new HashMap<String, Class<?>>();\n" +
+            "  static {\n" +
+            "#foreach($project in $DEPEND_LIST)" +
+            "    sActionMap.putAll(new $PACKAGE_NAME.${CLASS_NAME}$project());\n" +
+            "#end\n" +
+            "  }\n\n" +
+            "  @Override\n" +
+            "  public Class<?> getPage(String action) {\n" +
+            "    return sActionMap.get(action);\n" +
+            "  }\n\n}\n";
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
@@ -41,47 +71,24 @@ public class PageActionProcessor extends AbstractProcessor {
                 project = elementProject;
                 continue;
             }
-            messager.printMessage(Diagnostic.Kind.NOTE, PageActionProcessor.class.getSimpleName()
-                    + ":" + classElement.getQualifiedName() + "(\"" + classElement.getAnnotation(PageAction.class).value() + "\") ");
-            actionMap.put(classElement.getAnnotation(PageAction.class).value(), classElement.getQualifiedName().toString());
+            String action = classElement.getAnnotation(PageAction.class).value();
+            String page = classElement.getQualifiedName().toString();
+            messager.printMessage(Diagnostic.Kind.NOTE, TAG + ":" + page + "(\"" + action + "\") ");
+            actionMap.put(action, page);
         }
         if (project == null || project.name().isEmpty()) {
             return true;
         }
+        VelocityEngine ve = new VelocityEngine();
 
         // Generate map code for each project
         String pName = IPageActionMap.class.getPackage().getName() + ".impl";
         String cName = IPageActionMap.class.getSimpleName().substring(1) + "Impl" + project.name();
-        StringBuilder code = new StringBuilder();
-        code.append("// Generated code from PageActionProcessor. Do not modify!\n// Email: wenhua.ywh@alibaba-inc.com\npackage ");
-        code.append(pName);
-        code.append(";\n\n");
-        code.append("public final class ");
-        code.append(cName);
-        code.append(" extends " + HashMap.class.getName() + "<String, Class<?>> {\n\n  public ");
-        code.append(cName);
-        code.append("() {");
-        boolean isFirst = true;
-        for (Map.Entry<String, String> entry : actionMap.entrySet()) {
-            if (isFirst) {
-                isFirst = false;
-                code.append("\n");
-            }
-            code.append("    put(\"" + entry.getKey() + "\", " + entry.getValue() + ".class);\n");
-        }
-        code.append("  }\n\n}");
-        String codeString = code.toString();
-        messager.printMessage(Diagnostic.Kind.NOTE, cName + ":\n" + codeString);
-        try {
-            JavaFileObject fileObject = processingEnv.getFiler().createSourceFile(cName);
-            Writer writer = fileObject.openWriter();
-            writer.write(codeString);
-            writer.flush();
-            writer.close();
-        } catch (Exception e) {
-            messager.printMessage(Diagnostic.Kind.NOTE, PageActionProcessor.class.getSimpleName() + ":" + e);
-            System.exit(0);
-        }
+        VelocityContext vc = new VelocityContext();
+        vc.put("PACKAGE_NAME", pName);
+        vc.put("CLASS_NAME", cName);
+        vc.put("ACTION_MAP", actionMap);
+        createSourceFile(cName, ve, vc, TEMPLATE_MAP_LIBRARY);
 
         if (project.type() != PageActionProject.ProjectType.APPLICATION) {
             return true;
@@ -93,33 +100,40 @@ public class PageActionProcessor extends AbstractProcessor {
             dependenciesList.add(libName);
         }
         dependenciesList.add(project.name());
-        String pNameApp = pName;
         String cNameApp = IPageActionMap.class.getSimpleName().substring(1) + "Impl";
-        StringBuilder codeApp = new StringBuilder();
-        codeApp.append("// Generated code from PageActionProcessor. Do not modify!\n// Email: wenhua.ywh@alibaba-inc.com\npackage ");
-        codeApp.append(pNameApp);
-        codeApp.append(";\n\npublic final class ");
-        codeApp.append(cNameApp);
-        codeApp.append(" implements " + IPageActionMap.class.getName() + " {\n\n");
-        codeApp.append("  private static final " + Map.class.getName() + "<String, Class<?>> sActionMap = new " + HashMap.class.getName());
-        codeApp.append("<String, Class<?>>();\n  static {\n");
-        for (String name : dependenciesList) {
-            codeApp.append("    sActionMap.putAll(new " + pName + "." + cNameApp + name + "());\n");
-        }
-        codeApp.append("  }\n\n  @Override\n  public Class<?> getPage(String action) {\n    return sActionMap.get(action);\n  }\n\n}");
-        String codeAppString = codeApp.toString();
-        messager.printMessage(Diagnostic.Kind.NOTE, cNameApp + ":\n" + codeAppString);
-        try {
-            JavaFileObject fileObject = processingEnv.getFiler().createSourceFile(cNameApp);
-            Writer writer = fileObject.openWriter();
-            writer.write(codeAppString);
-            writer.flush();
-            writer.close();
-        } catch (Exception e) {
-            messager.printMessage(Diagnostic.Kind.NOTE, PageActionProcessor.class.getSimpleName() + ":" + e);
-            System.exit(0);
-        }
+        VelocityContext vcApp = new VelocityContext();
+        vcApp.put("PACKAGE_NAME", pName);
+        vcApp.put("CLASS_NAME", cNameApp);
+        vcApp.put("DEPEND_LIST", dependenciesList);
+        vcApp.put("INTERFACE_NAME", IPageActionMap.class.getName());
+        createSourceFile(cNameApp, ve, vcApp, TEMPLATE_MAP_APPLICATION);
         return true;
+    }
+
+    private void createSourceFile(String name, VelocityEngine engine, VelocityContext context, String template) {
+        Messager messager = processingEnv.getMessager();
+        StringWriter writer = new StringWriter();
+        engine.evaluate(context, writer, "", template);
+        String code = writer.toString();
+        messager.printMessage(Diagnostic.Kind.NOTE, name + ":\n" + code);
+        Writer fileWrite = null;
+        try {
+            fileWrite = processingEnv.getFiler().createSourceFile(name).openWriter();
+            fileWrite.write(code);
+            fileWrite.flush();
+        } catch (Exception e) {
+            messager.printMessage(Diagnostic.Kind.NOTE, TAG + ":" + e);
+            System.exit(0);
+        } finally {
+            try {
+                if (fileWrite != null) {
+                    fileWrite.close();
+                }
+            } catch (Exception ex) {
+                messager.printMessage(Diagnostic.Kind.NOTE, TAG + ":" + ex);
+                System.exit(0);
+            }
+        }
     }
 
 }
